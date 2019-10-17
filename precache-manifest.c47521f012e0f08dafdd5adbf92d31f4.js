8e774274026264383b810bb5459cd5db58404765
lerma1 <52028355+lerma1@users.noreply.github.com>
2019-10-17 13:26:46 +0700

    Add files via upload

---
+++ precache-manifest.c47521f012e0f08dafdd5adbf92d31f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58fa3c953ba11b66c8d31f03fcf5eb31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f2193a6b13a0497dee2",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "e1f6e2fa28e00996b5d9",
+    "url": "/static/css/main.33a67336.chunk.css"
+  },
+  {
+    "revision": "6f2193a6b13a0497dee2",
+    "url": "/static/js/2.3e42c48d.chunk.js"
+  },
+  {
+    "revision": "e1f6e2fa28e00996b5d9",
+    "url": "/static/js/main.66379f24.chunk.js"
+  },
+  {
+    "revision": "eee6cfce876c0204a5ac",
+    "url": "/static/js/runtime-main.82650e7f.js"
+  }
+]);
